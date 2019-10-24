29c72a95c76817a9b9b19313425a124a680daf22
liuqiang <lq573320328@163.com>
2019-10-24 21:45:50 +0800

    stark to queue

--- stark-queue/start-queue_test.go
+++ stark-queue/start-queue_test.go
@@ -10,3 +10,13 @@ func TestIsValidParentheses(t *testing.T) {
 	r := IsValidParentheses(s)
 	fmt.Println(r)
 }
+func TestMyQueue(t *testing.T) {
+	myQueue := ConstructorQueue()
+	myQueue.Push(5)
+	myQueue.Push(4)
+	n := myQueue.Pop()
+	fmt.Println(n)
+	v := myQueue.Peek()
+	fmt.Println(v)
+	fmt.Println(myQueue.Empty())
+}
