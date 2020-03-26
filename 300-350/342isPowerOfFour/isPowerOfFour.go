ea82ccc3c4247f8373e2a3f756c77ad6506f2b0a
liuqiang <lq573320328@163.com>
2020-03-26 09:11:20 +0800

    is power off four

---
+++ 300-350/342isPowerOfFour/isPowerOfFour.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+}
+
+/*
+给定一个整数 (32 位有符号整数)，请编写一个函数来判断它是否是 4 的幂次方。
+示例 1:
+输入: 16
+输出: true
+示例 2:
+输入: 5
+输出: false
+链接：https://leetcode-cn.com/problems/power-of-four
+*/
+// 方法1，一直除以4，直到有余数，如果num等于0还没有余数，返回true
+func isPowerOfFour(num int) bool {
+
+}
