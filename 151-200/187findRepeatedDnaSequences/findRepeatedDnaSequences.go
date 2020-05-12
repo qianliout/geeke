9d0d3c287b7ba26d7ae8e8b2770a87f5fc3bf644
liuqiang <lq573320328@163.com>
2020-05-12 23:00:15 +0800

    5-12

---
+++ 151-200/187findRepeatedDnaSequences/findRepeatedDnaSequences.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "AAAAAAAAAAA"
+	res := findRepeatedDnaSequences(s)
+	fmt.Println("res is ", res)
+}
+
+/*
+ 所有 DNA 都由一系列缩写为 A，C，G 和 T 的核苷酸组成，例如：“ACGAATTCCG”。在研究 DNA 时，识别 DNA
+ 中的重复序列有时会对研究非常有帮助。
+ 编写一个函数来查找 DNA 分子中所有出现超过一次的 10 个字母长的序列（子串）。
+ 示例：
+ 输入：s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+ 输出：["AAAAACCCCC", "CCCCCAAAAA"]
+*/
+func findRepeatedDnaSequences(s string) []string {
+	res := make([]string, 0)
+	if len(s) < 10 {
+		return res
+	}
+	exit := make(map[string]int)
+	ss := []byte(s)
+	for i := 0; i+10 <= len(ss); i++ {
+		exit[string(ss[i:i+10])] += 1
+	}
+	for word, i := range exit {
+		if i > 1 {
+			res = append(res, word)
+		}
+	}
+	fmt.Println(exit)
+	return res
+}
