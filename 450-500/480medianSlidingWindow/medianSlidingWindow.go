cdd7dd9cfc7e64f9f202588f51950396182d5685
liuqiang <lq573320328@163.com>
2020-05-16 11:35:18 +0800

    sliding windwon

--- 450-500/480medianSlidingWindow/medianSlidingWindow.go
+++ 450-500/480medianSlidingWindow/medianSlidingWindow.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"container/heap"
+	"fmt"
+
+	. "outback/leetcode/common/commonHeap"
+)
+
 func main() {
-	
+	//nums := []int{1, 3, -1, -3, 5, 3, 6, 7}
+	//nums := []int{1, 2, 3, 4, 2, 3, 1, 4, 2}
+	nums := []int{1, 3, -1, -3, 5, 3, 6, 7}          //[1.0,-1.0,-1.0,3.0,5.0,6.0]
+	window := medianSlidingWindow(nums, 3)
+	fmt.Println("res is ", window)
 }
 
 /*
@@ -27,7 +38,96 @@ func main() {
 与真实值误差在 10 ^ -5 以内的答案将被视作正确答案。
 */
 
-
+// 用两个堆,如果k是奇数,中位数在小顶堆上,如果k中偶数,值是两堆的平均数
 func medianSlidingWindow(nums []int, k int) []float64 {
+	res := make([]float64, 0)
+	if len(nums) < k {
+		return res
+	}
+	//先构建初始堆
+	rightHeap := make(MinHeap, 0)
+	leftHeap := make(MaxHeap, 0)
+	length := len(nums)
+	deleteMap := make(map[int]int)
+	// 用这两个变量来控制堆的平衡
+	leftNodeNum, rightNodeNum := 0, 0
 
+	// 先初始两个堆,先放在小堆,也就是后面的堆里
+	for i := 0; i < k; i++ {
+		heap.Push(&rightHeap, nums[i])
+		rightNodeNum++
+	}
+	for j := 0; j < (k+1)/2; j++ {
+		min := heap.Pop(&rightHeap).(int)
+		rightNodeNum--
+		heap.Push(&leftHeap, min)
+		leftNodeNum++
+	}
+	//fmt.Println("init",leftHeap, rightHeap)
+	for i := k; i < length; i++ {
+		// 先把中位数放在结果集中,因为这里是先加结果,所以在最后一次循环是,还有一组结果没有加,所以要加上
+		if k%2 == 0 {
+			res = append(res, float64(rightHeap[0]+leftHeap[0])/2)
+		} else {
+			res = append(res, float64(leftHeap[0]))
+		}
+
+		outNum, inNum := nums[i-k], nums[i]
+
+		// 移出元素判断是在那一边
+		if outNum <= leftHeap[0] {
+			leftNodeNum--
+		} else {
+			rightNodeNum--
+		}
+		deleteMap[outNum]++
+		// 加入元素
+		if len(leftHeap) > 0 && inNum <= leftHeap[0] {
+			heap.Push(&leftHeap, inNum)
+			leftNodeNum++
+		} else {
+			heap.Push(&rightHeap, inNum)
+			rightNodeNum++
+		}
+		// 加入元素后重新平衡
+		//fmt.Println("111111", nums[i], leftHeap, rightHeap, leftNodeNum, rightNodeNum)
+		// 因为我的设定是,如果是奇数,左边比右边多一个,所以右边是不能多于左边的
+		if rightNodeNum > leftNodeNum {
+			heap.Push(&leftHeap, heap.Pop(&rightHeap).(int))
+			rightNodeNum--
+			leftNodeNum++
+		}
+		// 左边比右边多的数大于一了
+		if leftNodeNum-rightNodeNum > 1 {
+			heap.Push(&rightHeap, heap.Pop(&leftHeap).(int))
+			rightNodeNum++
+			leftNodeNum--
+		}
+		//fmt.Println("2222222", leftHeap, rightHeap)
+		// 延迟删除
+		for len(leftHeap) > 0 {
+			if deleteMap[leftHeap[0]] > 0 {
+				p := heap.Pop(&leftHeap).(int)
+				deleteMap[p]--
+			} else {
+				break
+			}
+		}
+		for len(rightHeap) > 0 {
+			if deleteMap[rightHeap[0]] > 0 {
+				p := heap.Pop(&rightHeap).(int)
+				deleteMap[p]--
+			} else {
+				break
+			}
+		}
+	}
+
+	// 这里要注意,最后一条记录容易忘记
+	if k%2 == 0 {
+		res = append(res, float64(rightHeap[0]+leftHeap[0])/2)
+	} else {
+		res = append(res, float64(leftHeap[0]))
+	}
+	return res
 }
