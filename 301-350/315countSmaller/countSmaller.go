c1476457e4b8747e64672b295d85ee798e75ad6b
liuqiang <lq573320328@163.com>
2020-05-24 15:39:01 +0800

    树

--- 301-350/315countSmaller/countSmaller.go
+++ 301-350/315countSmaller/countSmaller.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"fmt"
+	. "outback/leetcode/common"
+)
+
 func main() {
+	nums := []int{1}
+	BitLeft(&nums, 1)
+	BitLeft(&nums, 1)
+	BitLeft(&nums, 2)
+	BitLeft(&nums, 2)
 
 }
 
@@ -17,5 +27,47 @@ nums[i] 右侧小于 nums[i] 的元素的数量。
 1 的右侧有 0 个更小的元素.
 */
 func countSmaller(nums []int) []int {
+	res := make([]int, 0)
+	if len(nums) == 0 {
+		return res
+	}
+	small := make([]int, 0)
+	m := make(map[int]int)
+	for i := len(nums) - 1; i >= 0; i-- {
+		r := bitsetLeft(&small, nums[i])
+		m[i] = r
+	}
+	for i := range nums {
+		res = append(res, m[i])
+	}
 
+	return res
+}
+
+//  使用二分查找,插入左边
+func bitsetLeft(nums *[]int, tar int) int {
+	if len(*nums) == 0 {
+		*nums = append(*nums, tar)
+		return 0
+	}
+	length := len(*nums)
+	left, right := 0, length-1
+	for left < right {
+		mid := left + (right-left)/2
+		if (*nums)[mid] < tar {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	//  检查最后
+	if (*nums)[left] == tar {
+
+	}
+
+	fmt.Println("num is ", *nums, left, tar)
+	r := (*nums)[left:]
+	*nums = append(append((*nums)[:left], tar), r...)
+
+	return left
 }
--- common/common.go
+++ common/common.go
@@ -1,6 +1,7 @@
 package common
 
 import (
+	"fmt"
 	"math"
 )
 
@@ -22,4 +23,33 @@ func Max(nums ...int) int {
 		}
 	}
 	return max
+}
+
+// 二分法,找到插入左边的位置,并插入左边
+func BitLeft(nums *[]int, tar int) int {
+	if len(*nums) == 0 {
+		*nums = append(*nums, tar)
+		return 0
+	}
+	length := len(*nums)
+	left, right := 0, length-1
+	for left < right {
+		mid := left + (right-left)/2
+		if (*nums)[mid] < tar {
+			left = mid + 1
+		} else {
+			right = mid
+		}
+	}
+	//  检查最后
+	if (*nums)[left] < tar {
+		*nums = append(*nums, tar)
+		return left
+	}
+
+	fmt.Println("num is ", *nums, left, tar)
+	r := (*nums)[left:]
+	*nums = append(append((*nums)[:left+1], tar), r...)
+
+	return left
 }
