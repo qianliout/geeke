f29c0b21a2e10f517d80f14e35ea120d5b7fe06d
liuqiang <lq573320328@163.com>
2020-06-22 22:01:58 +0800

    sql

---
+++ sql/196.sql
@@ -0,0 +1,28 @@
+/*
+编写一个 SQL 查询，来删除 Person 表中所有重复的电子邮箱，重复的邮箱里只保留 Id 最小 的那个。
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id 是这个表的主键。
+例如，在运行你的查询语句之后，上面的 Person 表应返回以下几行:
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+*/
+
+# 使用临时表
+delete from Person
+where Id not in (select id from (select min(Id) as  id from Person group by Email) t);
+
+
+# 使用自连接
+
+delete p1 from Person as p1,Person as p2 where p1.Email=p2.Email and p1.Id>p2.Id;
+
