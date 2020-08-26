80ceb0bddffe94c2713f859fb80178df009c4033
liuqiang <lq573320328@163.com>
2020-08-26 09:05:29 +0800

    init test

---
+++ second/init002/init003.go
@@ -0,0 +1,29 @@
+﻿package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	 h.initF()
+}
+
+type Hello struct {
+	initF func() int
+}
+
+var h Hello
+
+
+func SayHello() int {
+	fmt.Println("heello wordc")
+	return 3
+}
+
+func init() {
+	initF := func() int {
+		return SayHello()
+	}
+	fmt.Println("3333")
+	h = Hello{initF: initF}
+}
