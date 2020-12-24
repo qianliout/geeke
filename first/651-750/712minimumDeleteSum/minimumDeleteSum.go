eee36c08bcb3a971a691a78e1160592934bc5cd5
liuqianli <lq573320328@163.com>
2020-12-24 09:44:21 +0800

    window

---
+++ first/651-750/712minimumDeleteSum/minimumDeleteSum.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+
+	. "outback/leetcode/common"
+)
+
+func main() {
+	fmt.Println(minimumDeleteSum("sea", "eat"))
+}
+
+/*
+
+给定两个字符串s1, s2，找到使两个字符串相等所需删除字符的ASCII值的最小和。
+ 示例 1:
+输入: s1 = "sea", s2 = "eat"
+输出: 231
+解释: 在 "sea" 中删除 "s" 并将 "s" 的值(115)加入总和。
+在 "eat" 中删除 "t" 并将 116 加入总和。
+结束时，两个字符串相等，115 + 116 = 231 就是符合条件的最小和。
+ 示例 2:
+输入: s1 = "delete", s2 = "leet"
+输出: 403
+解释: 在 "delete" 中删除 "dee" 字符串变成 "let"，
+将 100[d]+101[e]+101[e] 加入总和。在 "leet" 中删除 "e" 将 101[e] 加入总和。
+结束时，两个字符串都等于 "let"，结果即为 100+101+101+101 = 403 。
+如果改为将两个字符串转换为 "lee" 或 "eet"，我们会得到 433 或 417 的结果，比答案更大。
+ 注意:
+ 0 < s1.length, s2.length <= 1000。
+ 所有字符串中的字符ASCII值在[97, 122]之间。
+*/
+
+func minimumDeleteSum(s1 string, s2 string) int {
+	ss1, ss2 := []byte(s1), []byte(s2)
+	// dp[i][j] 表示ss[:i][:j]的值，其中不包含i和j
+	dp := make(map[int]map[int]int)
+	// 初始化
+	sum1, sum2 := 0, 0
+	for i := 0; i <= len(ss1); i++ {
+		dp[i] = make(map[int]int)
+	}
+	for i := 1; i <= len(ss1); i++ {
+		sum1 += int(ss1[i-1])
+		dp[i][0] = sum1
+	}
+
+	for i := 1; i <= len(ss2); i++ {
+		sum2 += int(ss2[i-1])
+		dp[0][i] = sum2
+	}
+
+	// dp[0][0] = math.MaxInt64
+	for i := 1; i <= len(ss1); i++ {
+		for j := 1; j <= len(ss2); j++ {
+			if ss1[i-1] == ss2[j-1] {
+				dp[i][j] = dp[i-1][j-1]
+			} else {
+				dp[i][j] = Min(dp[i-1][j]+int(ss1[i-1]),
+					dp[i][j-1]+int(ss2[j-1]),
+					dp[i-1][j-1]+int(ss1[i-1])+int(ss2[j-1]))
+			}
+		}
+	}
+	return dp[len(ss1)][len(ss2)]
+}
---
+++ first/651-750/713numSubarrayProductLessThanK/numSubarrayProductLessThanK.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(window([]int{10, 5, 2, 6}, 100))
+	fmt.Println(window([]int{1, 1, 1, 1, 1}, 5))
+}
+
+/*
+给定一个正整数数组 nums。
+找出该数组内乘积小于 k 的连续的子数组的个数。
+示例 1:
+输入: nums = [10,5,2,6], k = 100
+输出: 8
+解释: 8个乘积小于100的子数组分别为: [10], [5], [2], [6], [10,5], [5,2], [2,6], [5,2,6]。
+需要注意的是 [10,5,2] 并不是乘积小于100的子数组。
+*/
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	if len(nums) <= 0 || k <= 0 {
+		return 0
+	}
+	sum := 0
+	// res := make([][]int, 0)
+	for i := 0; i < len(nums); i++ {
+		// path2 := []int{nums[i]}
+		// dfs(nums, 100, i, nums[i], path2, &sum, &res)
+		dfs2(nums, k, i, nums[i], &sum)
+	}
+	// fmt.Println(res)
+	return sum
+}
+
+// 注意的是连续的子数组
+func dfs(nums []int, k, start, path int, path2 []int, target *int, res *[][]int) {
+	if path < k {
+		*target = *target + 1
+		*res = append(*res, append([]int{}, path2...))
+	} else {
+		return
+	}
+	if start+1 < len(nums) {
+		if path*nums[start+1] < k {
+			path2 = append(path2, nums[start+1])
+			dfs(nums, k, start+1, path*nums[start+1], path2, target, res)
+			path2 = path2[:len(path2)-1]
+		}
+	}
+}
+
+// 这种方法可以用，但是会超时
+func dfs2(nums []int, k, start, path int, target *int) {
+	if path < k {
+		*target = *target + 1
+	} else {
+		return
+	}
+	if start+1 < len(nums) && path*nums[start+1] < k {
+		dfs2(nums, k, start+1, path*nums[start+1], target)
+	}
+}
+
+// 使用滑动窗口的解法
+func window(nums []int, k int) int {
+	if len(nums) == 0 || k <= 1 {
+		return 0
+	}
+	res, left, right, sum := 0, 0, 0, 1
+	for right < len(nums) {
+		sum *= nums[right]
+		// 这里有没有left<=right这个条件都行，是为什么呢?
+		for sum >= k && left <= right {
+			sum /= nums[left]
+			left++
+		}
+		res += right - left + 1
+		right++
+	}
+	return res
+}
